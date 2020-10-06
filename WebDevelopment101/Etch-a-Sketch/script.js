b8335b5234ee2e1426e1e9045c9ffc6b1373c6d0
DorskFR <dorsk.dev@gmail.com>
2020-10-06 17:57:35 +0900

    added brushes

--- WebDevelopment101/Etch-a-Sketch/script.js
+++ WebDevelopment101/Etch-a-Sketch/script.js
@@ -1,4 +1,7 @@
-const colorPalette = ['']
+let brush = "black";
+let fxColor = "#000000";
+const rainbow = ['#ff0000','#ffa500','#ffff00','#008000','#0000ff','#4b0082','#ee82ee'];
+let rbColor = 0;
 
 function generateGrid(gridsize) {
     const container = document.getElementById("container");
@@ -20,20 +23,44 @@ function generateGrid(gridsize) {
             cellCount++;
         }
     }
-
     container.appendChild(grid);
     document.querySelector("body").appendChild(container);
 }
 
 function randomHexColor() {
     let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + n.slice(0, 6);
+    let hexColor = '#' + n.slice(0, 6);
+    document.getElementById("colorpicker").style.backgroundColor = hexColor;
+    return hexColor;
+}
+
+function brushColor() {
+    switch(brush) {
+        case "black": return "#000000";
+            break;
+        case "rainbow": return rainbowColor();
+            break;
+        case "fixed": return fxColor;
+            break;
+        case "hexrandom": return randomHexColor();
+            break;
+    }
+}
+
+function rainbowColor() {
+    rbColor = rbColor + 1 < rainbow.length ? rbColor + 1 : 0;
+    document.getElementById("colorpicker").style.backgroundColor = rainbow[rbColor];
+    return rainbow[rbColor];
+}
+
+function fixedColor() {
+    fxColor = randomHexColor();
 }
 
 document.addEventListener("mouseover", event => {
     if (event.target.className === "gridcell") {
         console.log(event.target);
-        event.target.style.backgroundColor = randomHexColor();
+        event.target.style.backgroundColor = brushColor();
     }
 });
 
@@ -46,6 +73,24 @@ document.getElementById("resetbtn").addEventListener("click", event => {
     let container = document.getElementById("container");
     container.removeChild(container.childNodes[0]);
     generateGrid(gridsize);
+});
+
+document.getElementById("blackbtn").addEventListener("click", event => {
+    brush = "black";
+    document.getElementById("colorpicker").style.backgroundColor = "#000000";
+});
+
+document.getElementById("rainbowbtn").addEventListener("click", event => {
+    brush = "rainbow";
+});
+
+document.getElementById("fixrandombtn").addEventListener("click", event => {
+    brush = "fixed";
+    fixedColor();
+});
+
+document.getElementById("randomhexbtn").addEventListener("click", event => {
+    brush = "hexrandom";
 });
 
 generateGrid(16);
