a3b60dbc90f2036d4ad7a8e2a06cd8ad067603af
DorskFR <dorsk.dev@gmail.com>
2020-10-07 18:53:17 +0900

    calculator finished and factored

--- WebDevelopment101/Calculator/script.js
+++ WebDevelopment101/Calculator/script.js
@@ -4,6 +4,8 @@ let operator = "";
 let n1, n2, result;
 let calcstate = "wipeme";
 
+// Main function to call the mathematical operations
+
 function operate(operator, nb1, nb2) {
     nb1 = Number(nb1);
     nb2 = Number(nb2);
@@ -12,149 +14,150 @@ function operate(operator, nb1, nb2) {
         case "-": result = subtract(nb1, nb2); break;
         case "*": result = multiply(nb1, nb2); break;
         case "/": result = divide(nb1, nb2); break;
-        case "!": result = factorial(nb1); break;
         case "%": result = modulo(nb1, nb2); break;
+        case "^": result = power(nb1, nb2); break;
+        case "!": result = factorial(nb1); break;
+        case "√": result = squareroot(nb1); break;
     }
-    screen.textContent = result;
+    n1 = screen.textContent = result;
     if (nb2) {
-        sourcecode.textContent += `\n${nb1} ${operator} ${nb2} = ${result}`;
-    } else {
-        sourcecode.textContent += `\n${nb1} ${operator} = ${result}`;
+        sourcecode.textContent += `\n${nb1} ${operator} ${nb2} = ${result}\n`;
+    } else if (operator === "!") {
+        sourcecode.textContent += `\n${nb1}${operator} = ${result}\n`;
+    } else if (operator === "√") {
+        sourcecode.textContent += `\n${operator}${nb1} = ${result}\n`;
     }
-    
+
     sourcecode.scrollTop = sourcecode.scrollHeight;
     calcstate = "wipeme";
 }
 
-document.addEventListener("click", event => {
+// Capture inputs through the graphical interface
 
-    switch (event.target.className) {
-        case "gridnumber":
-            if (calcstate === "wipeme") {
-                calcstate = "leavemealone";
-                screen.textContent = "";
-            }
-            if (screen.textContent.length === 1 && Number(screen.textContent) === 0) {
-                if (Number(event.target.textContent) === 0) {
-                    //do nothing because its just leading zeros.
-                } else {
-                    screen.textContent = event.target.textContent;
-                }
-            } else {
-                screen.textContent += event.target.textContent;
-            }
-            break;
-        case "gridc":
-            calcstate = "leavemealone";
-            screen.textContent = "";
-            break;
-        case "gridce":
-            calcstate = "leavemealone";
-            screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
-            break;
-        case "griddot":
-            if (calcstate === "wipeme") {
-                calcstate = "leavemealone";
-                screen.textContent = "";
-            }
-            if (!screen.textContent.includes(".")) {
-                screen.textContent += ".";
-            } else if (!(/[\*\/\+\-\!\^\%]+.+\./.test(screen.textContent))) {
-                screen.textContent += ".";
-            }
-            break;
-        case "gridoperator":
-            calcstate = "leavemealone";
-            n1 = screen.textContent.replace(/[^0-9|.]/g, '');
-            operator = event.target.textContent;
-            screen.textContent = `${n1} ${operator} `;
-            break;
-        case "gridequal":
-            n2 = screen.textContent.slice(n1.length+3, screen.textContent.length);
-            if (operator != "" && n1 && n2 && n2 != ".") {
-                operate(operator, n1, n2);
-            } else if (operator === "!" && n1) {
-                operate(operator, n1, n2);
-            }
-            break;
+document.addEventListener("click", e => {
+
+    switch (e.target.className) {
+        case "gridnumber": calc_add_number(e.target.textContent); break;
+        case "gridc": calc_clearc(); break;
+        case "gridce": calc_clearce(); break;
+        case "griddot": calc_add_dot(); break;
+        case "gridoperator": calc_add_operator(e.target.textContent); break;
+        case "gridequal": calc_equal(); break;
     }
     screen.scrollLeft = screen.scrollWidth;
 });
 
-document.addEventListener("keydown", event => {
-    // if a digit, capture and input to screen
-    if (/[0-9]/.test(event.key)) {
-        if (screen.textContent.length === 1 && Number(screen.textContent) === 0) {
-            if (Number(event.key) === 0) {
-                //do nothing because its just leading zeros.
-            } else {
-                screen.textContent = event.key;
-            }
-        } else {
-            screen.textContent += event.key;
+// Capture inputs through the keyboard
+
+document.addEventListener("keydown", e => {
+    if (/[0-9]/.test(e.key)) { calc_add_number(e.key); }
+    else {
+        switch (e.key) {
+            case "Escape":
+            case "Delete": calc_clearc(); break;
+            case "Backspace": calc_clearce(); break;
+            case ".": calc_add_dot(); break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+            case "%":
+            case "!":
+            case "^": calc_add_operator(e.key); break;
+            case "=":
+            case "Enter": calc_equal(); break;
         }
-        screen.scrollLeft = screen.scrollWidth;
     }
-
-    // if not a digit, check against recognized keys
-    switch (event.key) {
-        case "Escape":
-        case "Delete":
-            screen.textContent = "";
-            break;
-        case "Backspace":
-            screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
-            break;
-        case ".":
-            if (!screen.textContent.includes(".")) {
-                screen.textContent += ".";
-            }
-            break;
-        case "+":
-        case "-":
-        case "*":
-        case "/":
-        case "%":
-        case "!":
-        case "^":
-            // operations
-            n1 = screen.textContent.replace(/[^0-9|.]/g, '');
-            operator = event.key;
-            screen.textContent = `${n1} ${operator} `;
-            break;
-        case "=":
-        case "Enter":
-            n2 = screen.textContent.slice(n1.length+3, screen.textContent.length);
-            if (operator != "" && n1 && n2 && n2 != ".") {
-                operate(operator, n1, n2);
-            } else if (operator === "!" && n1) {
-                operate(operator, n1, n2);
-            }
-            break;
-    }
-
+    screen.scrollLeft = screen.scrollWidth;
 });
 
-function add () {
-	return (arguments[0] + arguments[1]);
+// Functions that are called by both keyboard and mouse
+
+function calc_add_number(number) {
+    if (calcstate === "wipeme") {
+        calcstate = "leavemealone";
+        screen.textContent = "";
+    }
+    if (screen.textContent == 0 && number != 0) {
+            screen.textContent = number;
+    } else if(!(/[\!\√]+/.test(screen.textContent))) {
+        screen.textContent += number;
+    }
 }
 
-function subtract () {
-	return (arguments[0] - arguments[1]);
+function calc_clearc() { 
+    calcstate = "leavemealone";
+    screen.textContent = ""; 
 }
 
-function divide () {
-	return (arguments[0] / arguments[1]);
+function calc_clearce() {
+    calcstate = "leavemealone";
+    screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
 }
 
-function multiply () { 
-	return (arguments[0] * arguments[1]);
+function calc_add_dot() {
+    if (calcstate === "wipeme") {
+        calcstate = "leavemealone";
+        screen.textContent = "";
+    }
+    // no dot > dot and no operator > dot and operator and no dot > 2 dots
+    if (!(/[\!\√]+/.test(screen.textContent))) {
+        // do nothing because factorial or squareroot
+    } else if (!screen.textContent.includes(".")) {
+        // no dot
+        screen.textContent += ".";
+    } else if ((/[\*\/\+\-\!\^\%]+/.test(screen.textContent)) &&
+            !(/[\*\/\+\-\!\^\%]+.+\./.test(screen.textContent))) {
+        // dot and operator but no right dot
+        screen.textContent += ".";
+    }
 }
 
-function power() {
-	return Math.pow(arguments[0], arguments[1]);
+function calc_add_operator(op) {
+    calcstate = "leavemealone";
+    // if we have an operand and a number and we press a new operand we compute
+    if ((/[\*\/\+\-\!\^\%]+.+\d+/.test(screen.textContent)) ||
+        (/[\!\√]+/.test(screen.textContent))) {
+        calc_equal();
+        calcstate = "leavemealone";
+    } else {
+        n1 = screen.textContent.replace(/[^0-9|.]/g, '');
+        // if we have a dot but no number
+        if (!(/\d+/g.test(n1))) { n1 = 0; }
+    }
+    operator = op;
+    if (op === "!") {
+        screen.textContent = `${n1}${operator}`;
+    } else if (op === "√") {
+        screen.textContent = `${operator}${n1}`;
+    } else {
+        screen.textContent = `${n1} ${operator} `;
+    }
 }
 
+function calc_equal() {
+    if ((/[\!\√]+/.test(screen.textContent)) && n1) {
+        operate(operator, n1);
+    } else {
+        let operation = screen.textContent.split(' ');
+        n1 = operation[0];
+        operator = operation[1];
+        n2 = operation[2];
+        if (operator != "" && n1 && n2 && n2 != ".") {
+            operate(operator, n1, n2);
+        }
+    }
+}
+
+// Mathematical functions
+
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function divide(a, b) { return a / b; }
+function multiply(a, b) { return a * b; }
+function power(a, power) { return Math.pow(a, power); }
+function modulo(a, b) { return a % b; }
+function squareroot(a) { return Math.sqrt(a); }
 function factorial(n, total = 1) {
 	if (n === 0 || n === 1) {
 		return total;
@@ -164,6 +167,3 @@ function factorial(n, total = 1) {
 	}
 }
 
-function modulo() { 
-	return (arguments[0] % arguments[1]);
-}
