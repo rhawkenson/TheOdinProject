904470ee2bfcafa1264509ee03eedd1ce0c83f8a
DorskFR <dorsk.dev@gmail.com>
2020-10-13 13:31:41 +0900

    finished Hangman

---
+++ Ruby/014-dirfile.rb
@@ -0,0 +1,14 @@
+hash = Dir.glob("./../**/*").inject({}) do |hsh, fname|
+  ext = File.basename(fname).split('.')[-1].to_s.downcase
+  hsh[ext] ||= [0,0]
+  hsh[ext][0] += 1
+  hsh[ext][1] += File.size(fname)   
+  hsh
+end               
+File.open("file-analysis.txt", "w") do |f|
+  hash.each do |arr|
+    txt = arr.flatten.join("\t")
+     f.puts txt
+     puts txt
+  end
+end
